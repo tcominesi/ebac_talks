eabc25c9efb1b976bb6e2a223417467eac89e902
Tiago Antonio Cominesi <tcominesi@yahoo.com.br>
2025-04-02 16:52:47 -0300

    Inicio Commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,4 @@
-const dataDoEvento = new Date("Dec 12, 2024 19:00:00");
+const dataDoEvento = new Date("Jul 07, 2025 19:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
 const contaAsHoras = setInterval(function() {
